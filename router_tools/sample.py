3a6af6c5a9da759e043019e131ca9e34a5f6bd03
rwb <renweibo@gmail.com>
2016-12-10 14:18:40 +0800

    try Sphinx and read the docs

---
+++ router_tools/sample.py
@@ -0,0 +1,13 @@
+def sample():
+    """Return ``True`` if the the given *cls* supports the HTTP *method* found
+on the incoming HTTP request.
+
+:param cls: class associated with the request's endpoint
+:type cls: :class:`sandman.model.Model` instance
+:param string method: HTTP method of incoming request
+:param resource: *cls* instance associated with the request
+:type resource: :class:`sandman.model.Model` or None
+:rtype: bool
+
+"""
+    pass
