7f625c330e6dd1ebc9883f58ec92030e9996ebbc
rwb <renweibo@gmail.com>
2016-12-15 10:06:41 +0800

    messages for update

--- router_tools/__init__.py
+++ router_tools/__init__.py
@@ -1,5 +1,8 @@
 """router_tools - A simple tool for router maintain."""
+import logging
 
 __version__ = '0.1.6'
 __author__ = 'Ren Weibo <renweibo@gmail.com>'
 __all__ = []
+
+logging.basicConfig()
