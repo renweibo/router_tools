b377a886068772f9652591c047436f833a9ef4cf
rwb <renweibo@gmail.com>
2016-12-15 10:52:55 +0800

    Bump version: 0.1.7 → 0.1.8

--- router_tools/__init__.py
+++ router_tools/__init__.py
@@ -1,7 +1,7 @@
 """router_tools - A simple tool for router maintain."""
 import logging
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 __author__ = 'Ren Weibo <renweibo@gmail.com>'
 __all__ = []
 
